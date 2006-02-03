9abbff55540a90e31bb7f3e6831eb40c14b54115
Jiří Suchomel <jsuchome@suse.cz>
2006-02-03 12:04:08 +0000

    - show "Create Home" checkbox during installation (#147596) - save pam_winbind.so to /etc/pam.d/common-password (#146189)   (and revert behaviour introduced by #146653) - 2.13.5
    svn path=/trunk/samba-client/; revision=27574

--- src/SambaWinbind.pm
+++ src/SambaWinbind.pm
@@ -123,7 +123,7 @@ BEGIN{$TYPEINFO{AdjustPam}=["function","boolean","boolean"]}
 sub AdjustPam {
     my ($self, $on) = @_;
 
-    foreach my $db ("auth", "account","password") {
+    foreach my $db ("auth", "account") {
 	my $values = PamSettings->GetValues("pam_unix2", $db);
 	my @values = grep {$_ !~ /^call_modules=/} ($values ? @$values : ());
 	my @modules = map {split(",",$_)} grep {s/^call_modules=//} ($values ? @$values : ());
@@ -137,7 +137,38 @@ sub AdjustPam {
 	push @values, "call_modules=".join(",",@modules) if @modules;
 	PamSettings->SetValues("pam_unix2", $db, \@values);
     }
-
+    # for password changing, we must adapt /etc/pam.d/common-password (#146189)
+    my $file_path	= "/etc/pam.d/common-password";
+    my $write		= 1;
+    my @new_cont	= ();
+    if (FileUtils->Exists ($file_path)) {
+	my $cont 	= SCR->Read(".target.string", $file_path);
+	my $done	= 0;
+	foreach my $line (split(/\n/,$cont)) {
+	    if ($line =~ m/^#/ || $line eq "") {
+		push @new_cont, $line;
+		next;
+	    }
+	    if ($line =~ m/pam_winbind/) {
+		if ($on) {
+		    $write	= 0;
+		    last;
+		}
+	    } else {
+		if ($on && !$done) {
+		    push @new_cont, "password sufficient\tpam_winbind.so";
+		    $done	= 1;
+		}
+		push @new_cont, $line;
+	    }
+	}
+    }
+    else {
+	push @new_cont, "password sufficient\tpam_winbind.so"
+    }
+    if ($write) {
+	SCR->Write (".target.string", $file_path, join("\n",@new_cont));
+    }
     return TRUE;
 }
 
