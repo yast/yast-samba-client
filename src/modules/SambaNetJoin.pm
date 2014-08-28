389a155cfdf613901bfae2fe20fd4c779d46915b
David Disseldorp <ddiss@suse.de>
2014-08-28 15:30:47 +0200

    Support non-default ctdbd socket paths
    Read the ctdbd socket path from smb.conf, rather than assuming the
    default; (bnc#893154).
    - 3.1.12
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- src/modules/SambaNetJoin.pm
+++ src/modules/SambaNetJoin.pm
@@ -177,12 +177,18 @@ sub PrepareCTDB {
     # 7. Cleanup CTDB:
     CRMCall ("resource cleanup $rsc_id");
 
-    # 8. Wait until the unhealty status disappears.
+    # 8. Wait until the unhealthy status disappears.
     my $start   = time;
     my $wait    = 60; # 1 minute timeout
+    my $ctdbd_socket = SambaConfig->GlobalGetStr("ctdbd socket", undef);
+    my $ctdb_args = "";
+    if (defined $ctdbd_socket) {
+      $ctdb_args = "--socket=$ctdbd_socket";
+    }
 
     while (time<$start+$wait) {
-      my $out     = SCR->Execute(".target.bash_output", "/usr/bin/ctdb status");
+      my $out = SCR->Execute(".target.bash_output",
+			     "/usr/bin/ctdb $ctdb_args status");
       last if ($out->{"exit"} == 0);
       sleep (1); #0.5);
     }
